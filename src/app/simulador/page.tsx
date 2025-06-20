c3a7c56fd1f8e732cc01adfc605b8c8f4c2f52c5
sol <szapata@proyectowow.com>
2025-06-20 14:28:16 -0300

    base view Simulador

--- src/app/simulador/page.tsx
+++ src/app/simulador/page.tsx
@@ -1,7 +1,8 @@
+import Button from '@/components/shared/Button';
 import Footer from '@/components/shared/Footer';
+import HeroSection from '@/components/shared/HeroSection';
 import Navbar from '@/components/shared/NavBar';
 import SimuladorSection from '@/components/simulador/SimuladorSection';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function SimuladorPage() {
@@ -9,66 +10,43 @@ export default function SimuladorPage() {
     <main>
       <Navbar />
 
-      <section className="rounded-b-[50px] bg-light-aqua-green py-20 dark:bg-dark md:px-[100px] xl:px-[145px]">
+      <section className="rounded-b-[50px] py-20 dark:bg-dark md:px-[100px] xl:px-[145px]">
         <div
           className={`flex flex-col-reverse xl:flex-row items-center justify-between mx-auto`}
         >
           <div className="flex flex-col text-primary">
-            <h1 className="text-center xl:text-left font-encode-sans font-bold xl:font-black text-2xl xl:text-[45px] leading-[30px] xl:leading-[60px] text-primary mb-7 xl:mb-12 max-w-[590px]">
-              Simulador de inversiones
+            <h1 className="text-center xl:text-left font-encode-sans font-bold xl:font-black text-2xl xl:text-[45px] leading-[30px] xl:leading-[60px] text-primary-light mb-7 xl:mb-12 max-w-[590px]">
+              Simulador de fondos
             </h1>
-            <p className="text-center xl:text-left font-encode-sans font-normal text-[16px] xl:text-[20px] text-primary leading-5 xl:leading-6 px-[50px] md:w-[600px] xl:px-0 xl:mr-0">
-              Explicación del simulador Explicación del simulador Explicación
-              del simulador Explicación del simulador
+            <p className="text-center xl:text-left font-encode-sans font-normal text-[16px] xl:text-[20px] text-secondary leading-5 xl:leading-6 px-[50px] md:w-[600px] xl:px-0 xl:mr-0">
+              Usá el simulador de fondos para analizar y comparar diferentes
+              <br />
+              fondos según sus rendimientos pasados.
             </p>
           </div>
         </div>
       </section>
       <SimuladorSection />
-      <div className="py-20 grid gap-8 justify-center text-center relative bg-[#005A63] rounded-t-[50px] text-white px-6">
-        <h2 className="font-encode-sans font-black text-2xl lg:text-5xl">
-          ¿Querés invertir en dólar MEP?
-        </h2>
-        <p className="font-encode-sans text-md lg:text-xl mb-6">
-          Comprá y vendé activos, descubrí nuevas oportunidades y gestioná tu
-          dinero
-          <br className="hidden lg:block" /> de manera simple, rápida y segura.
-        </p>
-        <div className="flex justify-center gap-8">
-          {/* App Store */}
-          <Link
-            href="https://apps.apple.com/app/id6448729005"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center border border-gray-300 text-primary rounded-[10px] px-6 py-5 transition duration-200 hover:bg-white hover:shadow-[0_0_15px_rgba(0,0,0,0.35)]"
-          >
-            <Image
-              src="/home/AppStore.svg"
-              alt="Apple icon"
-              width={110}
-              height={110}
-              className="m-0 xl:mr-2"
-            />
-          </Link>
 
-          {/* Play Store */}
-          <Link
-            href="https://play.google.com/store/apps/details?id=io.btrader.prbu&hl"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="
-            flex items-center border border-gray-300 text-primary rounded-[10px] px-6 py-5 transition duration-200 hover:bg-white hover:shadow-[0_0_15px_rgba(0,0,0,0.35)] "
-          >
-            <Image
-              src="/home/PlayStore.svg"
-              alt="Play Store icon"
-              width={110}
-              height={110}
-              className="m-0 xl:mr-2"
-            />
-          </Link>
-        </div>
-      </div>
+      <section className="pt-6">
+        <HeroSection
+          title="¿Querés saber qué tipo de inversor sos?"
+          animation="/carteras/05_CarterasEficientes.json"
+          id="cartera"
+          bgColor="bg-[#2098A1]"
+          textColor="text-white"
+        >
+          <p className="text-center xl:text-left font-encode-sans font-normal text-[16px] xl:text-[20px] text-white leading-5 xl:leading-6 px-[50px] md:w-[555px] xl:px-0 xl:mr-0 mb-12">
+            ¿Sos más conservador o te gusta asumir riesgos? Hacé nuestro test en
+            minutos y encontrá la mejor estrategia de inversión para vos.
+          </p>
+          <div>
+            <Link href="/test-inversor" passHref>
+              <Button variant="sky">Hacé el test</Button>
+            </Link>
+          </div>
+        </HeroSection>
+      </section>
 
       <Footer />
     </main>
