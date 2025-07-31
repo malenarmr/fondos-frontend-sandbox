f3d5114db48b60766e31c0d672415fd350421370
Varela Cristian Facundo <cvarelagarcia@gmail.com>
2025-07-31 12:18:45 -0300

    institucional fixes

--- src/app/test-inversor/page.tsx
+++ src/app/test-inversor/page.tsx
@@ -19,7 +19,7 @@ export default function TestInversorPage() {
       <Navbar />
 
       {/* Hero section */}
-      <div className="px-4 md:px-0">
+      <div className="container mx-auto px-4 py-8">
         <HeroSectionGenerico
           title={heroData.title}
           description={heroData.description}
