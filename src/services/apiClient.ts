594732dd2ecacc323ba7fbcefc3ab43882d80ac6
Varela Cristian Facundo <cvarelagarcia@gmail.com>
2025-08-27 23:46:47 -0300

    Update API hostname from sandbox to production in next.config.ts and apiClient.ts

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_BASE_URL ||
-    // 'https://provincia.teocoop.site/api',
-    'https://provincia-sandbox-api.teocoop.site/api',
+    'https://provincia-prod-api.teocoop.site/api',
+  // 'https://provincia-sandbox-api.teocoop.site/api',
 
   headers: { 'Content-Type': 'application/json' },
 });
