7bbb0ff0f05322dbc12fcaff81811685835a0015
malenarmr <maalena456@gmail.com>
2026-05-29 17:10:29 -0300

    logo navbar

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -17,7 +17,7 @@ const Navbar: React.FC = () => {
           <Link href="/" className="w-full h-full">
             <div className="relative h-[65px] max-h-[65px] w-full">
               <Image
-                src="/Logo.png"
+                src="/Logo-original.png"
                 alt="logo"
                 fill
                 className="object-contain object-left"
