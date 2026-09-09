0faa35518eb4e0b9237d82c757e55415810a5d92
malenarmr <maalena456@gmail.com>
2026-09-09 18:14:15 -0300

    fORMULARIO

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import ContactBox from '@/components/contacto/ContactBox';
-// import Form from '@/components/contacto/Form';
+import Form from '@/components/contacto/Form';
 import HeroSectionGenerico from '@/components/shared/HeroSectionGenerico';
 import Navbar from '@/components/shared/NavBar';
 import Spinner from '@/components/shared/Spinner';
@@ -36,7 +36,7 @@ export default function ContactoPage() {
             <ContactBox contacto={contacto} />
           </div>
         </div>
-        {/* <Form /> */}
+        <Form />
       </div>
     </div>
   );
